7749e61aca5cb80afd0fefeb11adddc54a28d8af
Kasper Bertelsen <kasper@shapeshifters.xyz>
2017-08-03 13:18:24 +0200

    First shot

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width; initial-scale = 1.0;	maximum-scale=1.0; user-scalable=no" />
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Portfolio of Senior Art Director, Kasper Bertelsen">
+		<meta name="keywords" content="cd, creative, creative director, head of design, digital product, digital, art, director, kasper, bertelsen, copenhagen, denmark">
+		<meta name="author" content="Kasper Bertelsen">
+
+		<title>Creative Director &mdash; Product Designer, Kasper Bertelsen</title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/style.min.css">
+
+		<!-- Js -->
+		<!-- <script src="js/moments.js"></script> -->
+
+
+		<!-- Facebook and Twitter integration -->
+		<meta property="og:title" content=""/>
+		<meta property="og:image" content=""/>
+		<meta property="og:url" content=""/>
+		<meta property="og:site_name" content=""/>
+		<meta property="og:description" content=""/>
+		<meta name="twitter:title" content="" />
+		<meta name="twitter:image" content="" />
+		<meta name="twitter:url" content="" />
+		<meta name="twitter:card" content="" />
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.png">
--- index.php
+++ index.php
@@ -1,74 +1,77 @@
-<!DOCTYPE html>
-<html lang="pl">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PHP Landing Page</title>
+<?php include ("header.php"); ?>
 
-        <!-- build:css css/main.css -->
-        <link rel="stylesheet" href="css/main.css">
-        <!-- endbuild -->
-    </head>
-    <body>
+</head>
+<body>
+<div class="wrapper">
 
-        <header>
-            <div class="container">
-                <nav class="navbar">
-                    <div class="brand">
-                        <img src="http://placehold.it/150x50" alt="Brand">
-                    </div>
-                </nav>
-            </div>
-        </header>
+  <div class="container">
 
-        <main>
-            <div class="container">
-                <h1>Header one</h1>
-                <div class="col-sm-4">
-                    <div class="row">
-                        <p>Dolor officiis blanditiis ipsum consequatur quibusdam impedit Beatae quia perferendis eos amet optio quod. Nam sequi beatae numquam expedita nemo. Esse perspiciatis laboriosam quos itaque nulla. Optio in ad quas vel reprehenderit! Amet neque molestias necessitatibus officiis dicta Numquam delectus vero natus itaque natus. Quaerat amet ratione assumenda voluptatem minima</p>
-                    </div>
-                    <?php include_once('modal.php') ?>
-                </div>
-                <div class="col-sm-4 col-sm-offset-4">
-                    <?php include_once('form.php'); ?>
-                </div>
-            </div>
-        </main>
+      <div class="item-left item">
+          <div class="image__cover"></div>
+      </div>
 
-        <section>
-            <div class="container">
-                <h2>Nagłówek 2</h2>
-               <p>Ipsum atque adipisci explicabo placeat incidunt. Reiciendis fugit fugit exercitationem maiores animi quidem. Earum officia distinctio dolorem porro recusandae. Est reprehenderit numquam et consequuntur esse Aliquid asperiores voluptatibus sed reprehenderit sunt Hic porro officiis fugiat temporibus laborum? Nostrum minima est eos earum voluptatum dolorum Nulla alias esse asperiores harum tempora.</p>
-            </div>
-        </section>
+      <div class="item-right item">
+          <div class="text__wrap">
+            <lead class="lead">Kasper Bertelsen</lead>
+              <h1>Nerdy designer turned Co-founder & Creative Director</h1>
 
-        <section>
-            <div class="container">
-                <h2>Nagłówek 3</h2>
-                <br>
-                <div class="col-sm-3"><img src="http://placehold.it/150x150" alt="Client"></div>
-                <div class="col-sm-3"><img src="http://placehold.it/150x150" alt="Client"></div>
-                <div class="col-sm-3"><img src="http://placehold.it/150x150" alt="Client"></div>
-                <div class="col-sm-3"><img src="http://placehold.it/150x150" alt="Client"></div>
-            </div>
-        </section>
+              <p>
+              Digital born – raised by the internet. Emperor of design and
+              creative X-man. Likes to take on the underworld in a diving
+              suit. Comics nerd and all-things-tech fanboy. 11+ years as a
+              designer makes a quirky mind.
+              </p>
+              <p>Digital designer and lead Art Director turned Co-founder
+                & Creative Director at ShapeShifters Studio. Loves life, beers, action
+                sports and all things design.
+              </p>
 
-        <footer>
-            <hr/>
-            <div class="container">
-                Prawa autorskie
-            </div>
-        </footer>
+              <ul class="social">
+                <li><a href="http://twitter.com/kasperbertelsen/" target="_blank">
+                    <svg class="social-icon" version="1.1" id="Logo_FIXED" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                       width="250px" height="203.1px" viewBox="0 0 250 203.1" style="enable-background:new 0 0 250 203.1;" xml:space="preserve">
+                    <path d="M78.6,203.1c94.3,0,145.9-78.2,145.9-145.9c0-2.2,0-4.4-0.1-6.6c10-7.3,18.7-16.3,25.6-26.5c-9.4,4.1-19.3,6.9-29.5,8.1
+                      c10.7-6.4,18.7-16.5,22.5-28.4c-10.1,6-21.1,10.2-32.6,12.4c-19.4-20.7-51.9-21.7-72.6-2.2c-13.3,12.5-19,31.2-14.8,49
+                      C81.9,60.9,43.4,41.4,17.4,9.4C3.8,32.8,10.7,62.8,33.3,77.8c-8.2-0.2-16.1-2.4-23.3-6.4c0,0.2,0,0.4,0,0.6
+                      c0,24.4,17.2,45.4,41.2,50.3c-7.6,2.1-15.5,2.4-23.2,0.9c6.7,20.9,26,35.2,47.9,35.6c-18.2,14.3-40.6,22-63.7,22
+                      c-4.1,0-8.2-0.3-12.2-0.7C23.5,195.2,50.7,203.1,78.6,203.1"/>
+                    </svg>
+                </a></li>
+                <li><a href="https://www.dribbble.com/kasperbertelsen/" target="_blank">
+                      <svg class="social-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="49.4px" height="49.4px" viewBox="0 0 49.4 49.4" style="enable-background:new 0 0 49.4 49.4;" xml:space="preserve">
+                      <path d="M48.9,19.7c-0.3-1.6-0.8-3.2-1.4-4.6c-0.6-1.5-1.4-2.9-2.3-4.2c-0.9-1.3-1.9-2.5-3-3.7c-1.1-1.1-2.3-2.1-3.7-3
+                        c-1.3-0.9-2.7-1.7-4.2-2.3c-1.5-0.6-3.1-1.1-4.6-1.4c-1.6-0.3-3.3-0.5-5-0.5c-1.7,0-3.4,0.2-5,0.5c-1.6,0.3-3.2,0.8-4.6,1.4
+                        c-1.5,0.6-2.9,1.4-4.2,2.3c-1.3,0.9-2.5,1.9-3.7,3c-1.1,1.1-2.1,2.3-3,3.7c-0.9,1.3-1.7,2.7-2.3,4.2c-0.6,1.5-1.1,3.1-1.4,4.6
+                        C0.2,21.4,0,23,0,24.7c0,1.7,0.2,3.4,0.5,5c0.3,1.6,0.8,3.2,1.4,4.6c0.6,1.5,1.4,2.9,2.3,4.2c0.9,1.3,1.9,2.5,3,3.7
+                        c1.1,1.1,2.3,2.1,3.7,3c1.3,0.9,2.7,1.7,4.2,2.3c1.5,0.6,3.1,1.1,4.6,1.4c1.6,0.3,3.3,0.5,5,0.5c1.7,0,3.4-0.2,5-0.5
+                        c1.6-0.3,3.2-0.8,4.6-1.4c1.5-0.6,2.9-1.4,4.2-2.3c1.3-0.9,2.5-1.9,3.7-3c1.1-1.1,2.1-2.3,3-3.7c0.9-1.3,1.7-2.7,2.3-4.2
+                        c0.6-1.5,1.1-3.1,1.4-4.6c0.3-1.6,0.5-3.3,0.5-5C49.4,23,49.3,21.4,48.9,19.7z M45.8,24.5c-0.3-0.1-7.4-1.6-14.7-0.7
+                        c-0.1-0.4-0.3-0.7-0.5-1.1c-0.4-1-0.9-2.1-1.4-3.1c8.3-3.4,11.7-8.3,11.8-8.4C44,14.9,45.8,19.5,45.8,24.5z M38.7,8.9
+                        c-0.1,0.1-3.1,4.7-11,7.6C24,9.9,20.1,4.6,19.7,4.2c1.6-0.4,3.3-0.6,5-0.6C30.1,3.6,34.9,5.6,38.7,8.9z M19.7,4.2
+                        C19.7,4.2,19.7,4.2,19.7,4.2C19.7,4.2,19.6,4.2,19.7,4.2C19.6,4.2,19.6,4.2,19.7,4.2z M15.7,5.6c0.3,0.4,4.2,5.7,7.8,12.2
+                        c-10.1,2.7-18.9,2.6-19.5,2.6C5.4,13.8,9.8,8.4,15.7,5.6z M3.6,24.7c0-0.2,0-0.4,0-0.7c0.4,0,10.8,0.2,21.7-3
+                        c0.6,1.2,1.2,2.4,1.7,3.6c-0.3,0.1-0.6,0.2-0.8,0.3C14.8,28.6,9,38.8,9,38.8s0,0,0,0C5.7,35.1,3.6,30.1,3.6,24.7z M24.7,45.8
+                        c-5.1,0-9.8-1.8-13.4-4.8c0.3,0.2,0.5,0.4,0.5,0.4s4-8.8,16.6-13.2c0,0,0.1,0,0.1,0c3,7.7,4.2,14.2,4.5,16
+                        C30.4,45.2,27.6,45.8,24.7,45.8z M36.5,42.2c-0.2-1.3-1.3-7.5-4.1-15.1c6.8-1.1,12.7,0.8,13.2,0.9C44.6,34,41.3,39,36.5,42.2z"/>
+                      </svg>
+                </a></li>
+                <li><a href="https://www.linkedin.com/in/kasperbertelsen/" target="_blank">
+                    <svg class="social-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                       width="49.4px" height="49.7px" viewBox="0 0 49.4 49.7" style="enable-background:new 0 0 49.4 49.7;" xml:space="preserve">
+                      <path d="M26.6,22.4L26.6,22.4C26.6,22.4,26.5,22.4,26.6,22.4L26.6,22.4z"/>
+                      <path d="M45.8,0H3.7C1.6,0,0,1.6,0,3.6v42.6c0,2,1.6,3.6,3.7,3.6h42.1c2,0,3.7-1.6,3.7-3.6V3.6C49.4,1.6,47.8,0,45.8,0z M15,41.6
+                        H7.5V19.2H15V41.6z M11.2,16.1L11.2,16.1c-2.6,0-4.2-1.7-4.2-3.9c0-2.2,1.7-3.9,4.2-3.9c2.6,0,4.1,1.7,4.2,3.9
+                        C15.5,14.4,13.9,16.1,11.2,16.1z M41.9,41.6h-7.5v-12c0-3-1.1-5.1-3.8-5.1c-2.1,0-3.3,1.4-3.8,2.7c-0.2,0.5-0.2,1.1-0.2,1.8v12.5
+                        h-7.5c0,0,0.1-20.4,0-22.5h7.5v3.2c1-1.5,2.8-3.7,6.7-3.7c4.9,0,8.6,3.2,8.6,10.1L41.9,41.6L41.9,41.6z"/>
+                    </svg>
+                </a></li>
+              </ul>
+          </div>
+      </div>
 
-        <!-- build:js scripts/vendors.js -->
-        <script src="vendor/jquery-3.2.1.min.js"></script>
-        <script src="vendor/bootstrap-3.37/javascripts/bootstrap.min.js"></script>
-        <script src="vendor/jquery.c3r.tooltip.js"></script>
-        <script src="vendor/jquery.c3r.validate.js"></script>
-        <!-- endbuild -->
-        <!-- build:js scripts/main.js -->
-        <script src="js/main.js"></script>
-        <!-- endbuild -->
-    </body>
+  </div>
+
+</div>
+</body>
 </html>
